6aa53fd44f6d75dfdd865f240862d547b74f800d
Корякін Захар <zakhar.koriakin@nure.ua>
2024-11-18 12:14:10 +0200

    Update MainActivity.java

--- PractTask1/android-pzpi-23-8-koriakin-zakhar-pract-task1/java/com/example/zakhar_koryakin_pzpi_23_8/MainActivity.java
+++ PractTask1/android-pzpi-23-8-koriakin-zakhar-pract-task1/java/com/example/zakhar_koryakin_pzpi_23_8/MainActivity.java
@@ -1,60 +1,30 @@
 package com.example.zakhar_koryakin_pzpi_23_8;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Життевий цикл", "onCreate");
 
-        TextView textView = findViewById(R.id.text_view);
-        textView.setText("Task 1");
-    }
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.text);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("Життевий цикл", "onStart");
-    }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("Життевий цикл", "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("Життевий цикл", "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("Життевий цикл", "onStop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("Життевий цикл", "onRestart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("Життевий цикл", "onDestroy");
+                textView.setText("\uD83D\uDE0A\uD83D\uDE0A\uD83D\uDE0A");
+            }
+        });
     }
 }
