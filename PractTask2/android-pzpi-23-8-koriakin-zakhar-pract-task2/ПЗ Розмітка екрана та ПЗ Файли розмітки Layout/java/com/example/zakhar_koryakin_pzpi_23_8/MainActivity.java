4915b0f079f15096ccf88af6873cf4fd0c76a8b8
Корякін Захар <zakhar.koriakin@nure.ua>
2024-11-18 12:17:35 +0200

    Update MainActivity.java

--- PractTask2/android-pzpi-23-8-koriakin-zakhar-pract-task2/ПЗ Розмітка екрана та ПЗ Файли розмітки Layout/java/com/example/zakhar_koryakin_pzpi_23_8/MainActivity.java
+++ PractTask2/android-pzpi-23-8-koriakin-zakhar-pract-task2/ПЗ Розмітка екрана та ПЗ Файли розмітки Layout/java/com/example/zakhar_koryakin_pzpi_23_8/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.zakhar_koryakin_pzpi_23_8;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,45 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Життевий цикл", "onCreate");
-
-        TextView textView = findViewById(R.id.text_view);
-        textView.setText("Task 1");
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("Життевий цикл", "onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("Життевий цикл", "onResume");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("Життевий цикл", "onPause");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("Життевий цикл", "onStop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("Життевий цикл", "onRestart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("Життевий цикл", "onDestroy");
+    public void onButtonClick(View view) {
+        Toast.makeText(this, "Button Clicked!", Toast.LENGTH_SHORT).show();
     }
 }
